434fbb9730693d711420566995bf883c86967be8
mirzasafaraz <59474786+mirzavvip@users.noreply.github.com>
2020-01-03 20:16:03 +0700

    Create 630a.cpp

---
+++ codeforces/630a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long  a, b, x;
+
+int main(){
+	cin >> x;
+	if(x == 1){
+		cout << 5 << endl;
+		return 0;
+	}
+	cout << 25 << endl;
+	return 0;
+}
